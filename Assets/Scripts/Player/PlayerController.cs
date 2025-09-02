4100b0b27e514d90f9a24e6ea56950262e567944
gyro1515 <gyro1515@naver.com>
2025-09-02 09:55:52 +0900

    [Feat] 플레이어 인풋시스템 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : BaseController
 {
-    Player player;
+    public PlayerInput Input { get; private set; }
+    public PlayerInput.PlayerActions PlayerActions { get; private set; }
+
     protected override void Awake()
     {
         base.Awake();
         player = GetComponent<Player>();
+        input = new PlayerInput();
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
     }
 }
