c7362445b140065d3e59fcccc9cc5f895e0aae64
gyro1515 <gyro1515@naver.com>
2025-09-02 14:38:21 +0900

    [Chore] 사운드 매니저 설명 추가

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 
 public class SoundManager : SingletonMono<SoundManager>
 {
+    // 인스펙터 창에 드러난 것들은 다 확인용입니다.
+    // ***자동으로 생성돼서, 인스펙터 창에서 설정 못합니다***
     [field: Header("사운드 설정")]
     [field: SerializeField][field: Range(0f, 1f)] public float SoundEffectVolume { get; set; } = 1f;
     [field: SerializeField][field: Range(0f, 1f)] public float MusicVolume { get; set; } = 1f;
     private AudioSource musicAudioSource;
-    public AudioClip musicClip;
+    [Header("배경 음악")]
+    [SerializeField] AudioClip musicClip;
     [Header("효과음용 프리팹")]
     [SerializeField] SoundSource soundSourcePrefab;
     List<SoundSource> soundSources = new List<SoundSource>(); // 사운드 소스 오브젝트 풀
@@ -25,7 +28,8 @@ public class SoundManager : SingletonMono<SoundManager>
     }
     private void Start()
     {
-        ChangeBackGroundMusic(musicClip);
+        //아마 다른 곳에서 호출할 듯합니다. 씬로더라든지
+        //ChangeBackGroundMusic(musicClip);
     }
 
     public void ChangeBackGroundMusic(AudioClip clip)
@@ -37,16 +41,14 @@ public class SoundManager : SingletonMono<SoundManager>
 
     public static void PlayClip(AudioClip clip)
     {
-        bool isPlayed = false;
         for(int i = 0; i < Instance.soundSources.Count; i++)
         {
             if (Instance.soundSources[i].gameObject.activeSelf) continue;
             // 비활성화 찾아서 재생시키기
             Instance.soundSources[i].Play(clip, Instance.SoundEffectVolume);
-            isPlayed = true;
-            break;
+            return;
         }
-        if (isPlayed) return;
+
         // 재생할 게 없다면 생성하기
         SoundSource obj = Instantiate(Instance.soundSourcePrefab);
         obj.Play(clip, Instance.SoundEffectVolume);
