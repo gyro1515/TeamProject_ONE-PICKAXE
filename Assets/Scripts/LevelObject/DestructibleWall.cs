40437f8dc616ce74b286031d1b2e7d22a702d12a
heegin92 <kimheegin@gmail.com>
2025-09-04 20:50:48 +0900

    [Fix] 상호작용

--- Assets/Scripts/LevelObject/DestructibleWall.cs
+++ Assets/Scripts/LevelObject/DestructibleWall.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class DestructibleWall : InteractableObject, IDamageable
 {
     public int currentHealth = 3;
+    // 무기 레이어를 설정할 변수
+    public LayerMask weaponLayer;
 
-    // 매개변수를 받는 Interaction 메서드를 override하여 구현
     public override void Interaction(GameObject other)
     {
+        Debug.Log("벽에 " + other.name + "이(가) 닿았습니다. 레이어: " + other.layer);
+        Debug.Log("유효한 레이어 마스크 값: " + weaponLayer.value);
 
-            // 이 벽 오브젝트에 데미지를 입힘
+        // 충돌한 오브젝트의 레이어가 'weaponLayer'에 포함되는지 확인
+        if (((1 << other.layer) & weaponLayer) != 0)
+        {
+            Debug.Log("유효한 레이어입니다. 데미지를 입힙니다.");
             TakeDamage(1);
-        
+        }
+        else
+        {
+            Debug.Log("유효하지 않은 레이어입니다. 데미지가 적용되지 않습니다.");
+        }
     }
 
-    // IDamageable 인터페이스 구현
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
@@ -27,6 +36,8 @@ public class DestructibleWall : InteractableObject, IDamageable
 
     private void DestroyWall()
     {
-        gameObject.SetActive(false);
+        // `Destroy(gameObject);`를 사용하여 오브젝트를 완전히 제거합니다.
+        // `gameObject.SetActive(false);`는 오브젝트를 비활성화만 할 뿐 파괴하지 않습니다.
+        Destroy(gameObject);
     }
 }
