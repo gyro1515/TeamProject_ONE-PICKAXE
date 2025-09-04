c8d92dd0cde11c8642a5646026fce7ba8bd70c2f
heegin92 <kimheegin@gmail.com>
2025-09-04 21:12:49 +0900

    [Fix] 수정중

--- Assets/Scripts/LevelObject/DestructibleWall.cs
+++ Assets/Scripts/LevelObject/DestructibleWall.cs
@@ -1,25 +1,17 @@
 using UnityEngine;
 
+// InteractableObject는 OnCollisionEnter를 사용하도록 변경해야 합니다.
 public class DestructibleWall : InteractableObject, IDamageable
 {
     public int currentHealth = 3;
-    // 무기 레이어를 설정할 변수
     public LayerMask weaponLayer;
 
     public override void Interaction(GameObject other)
     {
-        Debug.Log("벽에 " + other.name + "이(가) 닿았습니다. 레이어: " + other.layer);
-        Debug.Log("유효한 레이어 마스크 값: " + weaponLayer.value);
-
-        // 충돌한 오브젝트의 레이어가 'weaponLayer'에 포함되는지 확인
+        // OnCollisionEnter 이벤트는 콜라이더가 닿는 순간 호출됩니다.
         if (((1 << other.layer) & weaponLayer) != 0)
         {
-            Debug.Log("유효한 레이어입니다. 데미지를 입힙니다.");
             TakeDamage(1);
-        }
-        else
-        {
-            Debug.Log("유효하지 않은 레이어입니다. 데미지가 적용되지 않습니다.");
         }
     }
 
@@ -36,8 +28,6 @@ public class DestructibleWall : InteractableObject, IDamageable
 
     private void DestroyWall()
     {
-        // `Destroy(gameObject);`를 사용하여 오브젝트를 완전히 제거합니다.
-        // `gameObject.SetActive(false);`는 오브젝트를 비활성화만 할 뿐 파괴하지 않습니다.
         Destroy(gameObject);
     }
 }
