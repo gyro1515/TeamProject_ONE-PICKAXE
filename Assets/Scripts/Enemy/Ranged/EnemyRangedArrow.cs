7ea566b5768d75807d73f9d609743f4a754dbec4
gyro1515 <gyro1515@naver.com>
2025-09-04 22:35:53 +0900

    [Feat] 화살 히트 파티클 추가, 발사체 오브젝트 풀링이랑 같이 작동하도록 로직 수정

--- Assets/Scripts/Enemy/Ranged/EnemyRangedArrow.cs
+++ Assets/Scripts/Enemy/Ranged/EnemyRangedArrow.cs
@@ -9,16 +9,18 @@ public class EnemyRangedArrow : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float range;
     [SerializeField] protected AudioClip hitSoundClip;
+    [SerializeField] protected ParticleSystem hitParticle;
 
     Rigidbody2D rb2D;
+    BoxCollider2D col;
     int damage = 0;
     float dir = 0;
     float startPosX = 0f;
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        enabled = false;
-        gameObject.SetActive(false);
+        col = GetComponent<BoxCollider2D>();
+        SetDeactive();
     }
     private void FixedUpdate()
     {
@@ -33,22 +35,47 @@ public class EnemyRangedArrow : MonoBehaviour
         else dir = 1f;
         gameObject.transform.position = spawnPos;
         this.damage = damage;
-        gameObject.SetActive(true);
-        enabled = true;
         startPosX = spawnPos.x;
+        SetActive();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 플레이어만 맞도록 콜라이더 설정
         IDamageable damageable = collision.GetComponent<IDamageable>();
         damageable?.TakeDamage(damage);
-        SetDeactive();
+        ArrowHit();
+    }
+    void SetActive()
+    {
+        col.enabled = true;
+        spriteRenderer.gameObject.SetActive(true);
+        gameObject.SetActive(true);
+        enabled = true;
     }
     void SetDeactive()
     {
-        if (hitSoundClip) SoundManager.PlayClip(hitSoundClip);
         gameObject.SetActive(false);
         enabled = false;
+        col.enabled = false;
         dir = 0f;
+    }
+    void ArrowHit()
+    {
+        // 히트 사운드 출력
+        if (hitSoundClip) SoundManager.PlayClip(hitSoundClip);
+        // 히트 파티클 출력
+        hitParticle?.Play();
+        // 이동 멈추기
+        dir = 0f;
+        // 화살 이미지 끄기
+        spriteRenderer.gameObject.SetActive(false);
+        col.enabled = false;
+        StartCoroutine(StartDeacive());
+    }
+    IEnumerator StartDeacive()
+    {
+        // 파티클 끝나면 비활성화 하기
+        yield return new WaitForSeconds(hitParticle.main.duration);
+        SetDeactive();
     }
 }
