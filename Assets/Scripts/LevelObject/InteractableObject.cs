807402cf7b781e598ac0770684120d08cfb61276
gyro1515 <gyro1515@naver.com>
2025-09-05 13:10:19 +0900

    [Fix] 기본 문과, 부셔지는 문에 적이 통과하지 못하도록 콜라이더 레이어 수정

--- Assets/Scripts/LevelObject/InteractableObject.cs
+++ Assets/Scripts/LevelObject/InteractableObject.cs
@@ -9,11 +9,14 @@ public abstract class InteractableObject : MonoBehaviour
     // OnTriggerEnter 대신 OnCollisionEnter를 사용
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("OnCollisionEnter");
         Interaction(collision.gameObject);
     }
     // OnTriggerEnter2D를 사용하면 isTrigger 설정으로 더 유연하게 충돌을 처리할 수 있습니다.
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("OnTriggerEnter2D");
+
         // 충돌한 오브젝트에서 InteractableObject의 상호작용을 실행합니다.
         // 플레이어에 한정하지 않고 모든 IDamageable 대상과 상호작용할 수 있게 설계합니다.
         Interaction(other.gameObject);
